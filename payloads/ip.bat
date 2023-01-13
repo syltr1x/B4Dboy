e8774c2bacbb4f2891c6319d451b99e19a25e24d
r4yX <108579252+r4yX@users.noreply.github.com>
2023-01-13 03:12:53 -0300

    Delete payloads directory

--- payloads/ip.bat
+++
@@ -1,5 +0,0 @@
-@ECHO off
-
-md %AppData%\.badboy
-
-for /f "delims=" %%i in ('curl -s https://api.ipify.org') do for /f "delims=" %%a in ('curl -s ipinfo.io/%%i?token=a70e36470018f5') do echo %%a >> "data.json"
--- payloads/ip.sh
+++
@@ -1,3 +0,0 @@
-ip=$("curl -s https://api.ipify.org")
-data=$("curl -s ipinfo.io/$ip?token=a70e36470018f5")
-echo $data >> "data.json"
